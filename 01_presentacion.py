f276d4f3fb219ef5415d6c4b855f0fbfda713493
Magnusmajo <dralexis19003227@gmail.com>
2025-08-15 15:51:03 -0300

    tarea 1

--- 01_presentacion.py
+++ 01_presentacion.py
@@ -1,12 +1,16 @@
 """ 1. Presentación personal (prints)
 Crea 01_presentación.py que imprima 3 líneas: nombre, edad y para qué querés aprender a programar."""
 
-nombre = input("Cómo te llamas?: ")
-try:
-    edad = int(input("Cuantos años tienes?: "))
-except ValueError:
-    print("Por favor ingresa un número válido para la edad. Solo números.")
-    edad = int(input("Cuantos años tienes?: "))
-motivo = input("Para qué quieres aprender a programar?: ")
+nombre = input("¿Cómo te llamas?: ")
 
-print(f"Hola!, mi nombre es {nombre}, tengo {edad} años de edad y quiero aprender a programar porque {motivo}.")
+while True:
+    edad_input = input("¿Cuántos años tienes?: ")
+    try:
+        edad = int(edad_input)
+        break
+    except ValueError:
+        print("❌ Por favor, ingresa solo números enteros.")
+
+motivo = input("¿Para qué quieres aprender a programar?: ")
+
+print(f"Hola, mi nombre es {nombre}, tengo {edad} años y quiero aprender a programar porque {motivo}.")
