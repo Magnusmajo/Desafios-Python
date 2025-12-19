32f88d60bc26ecc9def0d91282ef175184435c07
Magnusmajo <dralexis19003227@gmail.com>
2025-12-19 14:59:37 -0300

    Ejercicio 6

---
+++ SEMANA1/DIA_4/ejercicio6.py
@@ -0,0 +1,10 @@
+"""Ejercicio 6 — Pensamiento backend
+
+Crea una función es_mayor_edad(edad) que devuelva True o False."""
+
+def es_mayor_edad(edad):
+    """Devuelve True si la edad es mayor o igual a 18, False en caso contrario."""
+    return edad >= 18
+
+# Ejemplos de uso
+print(es_mayor_edad(12)) 
