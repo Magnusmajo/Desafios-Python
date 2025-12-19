7bc118d53b28f1515e73af7b40d51d8edba36dc5
Magnusmajo <dralexis19003227@gmail.com>
2025-12-19 14:19:28 -0300

    Ejercicio1

---
+++ SEMANA1/DIA_4/ejercicio1.py
@@ -0,0 +1,9 @@
+""" Ejercicio 1 — Básico
+
+Crea una función llamada mostrar_bienvenida que imprima un mensaje de bienvenida."""
+
+def mostrar_bienvenida():
+    print("¡Bienvenido a nuestro programa!")
+
+# Llamada a la función para mostrar el mensaje de bienvenida
+mostrar_bienvenida()
--- probar.py
+++ probar.py
@@ -1,4 +1,8 @@
-x = 0
-while x < 3:
-    print(x)
-    x += 1
+def agregar_item(item, lista=[]):
+    lista.append(item)
+    return lista
+
+items = agregar_item('manzana, banana, coco')
+print(items)  # Salida esperada: ['manzana, banana, coco']
+items = agregar_item('naranja')
+print(items)  # Salida esperada: ['naranja']
