c6f49196b8648385383e1b00cf8d695993b72118
Magnusmajo <dralexis19003227@gmail.com>
2026-01-07 18:59:19 -0300

    Bucles

---
+++ SEMANA1/EJERCICIOS_SEMANA_1/BUCLES/bucles3.py
@@ -0,0 +1,7 @@
+"""Imprime los números del 10 al 1."""
+
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
+
+# Regla de [range(inicio, fin, paso)]
---
+++ SEMANA1/EJERCICIOS_SEMANA_1/BUCLES/bucles4.py
@@ -0,0 +1,4 @@
+"""Imprime la tabla del 5."""
+
+for i in range(1, 11):
+    print(i * 5)
