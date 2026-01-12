fc87f66052f1ae265a8a5d9ca7a6315390dbb799
Magnusmajo <dralexis19003227@gmail.com>
2026-01-12 15:19:01 -0300

    Password

---
+++ SEMANA1/EJERCICIOS_SEMANA_1/While/suma_todo.py
@@ -0,0 +1,15 @@
+"""Pide números y suma todos hasta que el usuario ingrese un número negativo."""
+
+suma = 0
+
+while True:
+    try:
+        num = float(input("Ingresa un número: "))
+        if num >= 0:
+            suma = suma + num
+            print(suma)
+        else:
+            print("Número negativo: Chao")
+            break
+    except ValueError:
+        print("Debes ingresar un número válido")
