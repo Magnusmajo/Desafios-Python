2c24dfed4d297abaa1cce8a37540599e22771c43
Magnusmajo <dralexis19003227@gmail.com>
2026-01-16 17:09:23 -0300

    Recibir nota

--- SEMANA1/EJERCICIOS_SEMANA_1/FUNCIONES/Logicas/aprobado_o_no.py
+++ SEMANA1/EJERCICIOS_SEMANA_1/FUNCIONES/Logicas/aprobado_o_no.py
@@ -2,19 +2,34 @@
 
 def recibir_nota(nota):
     if not 0 <= nota <= 100:
-        return None
+        raise ValueError("Nota fuera de rango")
     return "Aprobado" if nota >= 60 else "Reprobado"
 
 while True:
     try:
         nota = float(input("Ingrese la nota del estudiante: "))
-        resultado = recibir_nota(nota)
-        
-        if resultado is None:
-            print("Nota fuera de rango (0 - 100)")
-            continue
-        
-        print(resultado)
+        print(recibir_nota(nota))
         break
-    except ValueError:
-        print("Ingresa un número válido")
+    except ValueError as e:
+        print(e)
+
+
+# <--- Validación fuera de la funció --->
+# def recibir_nota(nota):
+#     if not 0 <= nota <= 100:
+#         return None
+#     return "Aprobado" if nota >= 60 else "Reprobado"
+
+# while True:
+#     try:
+#         nota = float(input("Ingrese la nota del estudiante: "))
+#         resultado = recibir_nota(nota)
+        
+#         if resultado is None:
+#             print("Nota fuera de rango (0 - 100)")
+#             continue
+        
+#         print(resultado)
+#         break
+#     except ValueError:
+#         print("Ingresa un número válido")
